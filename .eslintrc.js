7d5f56ef79a5e62e53295833e1b289e0a06eba7c
flibbertz <yasha.vysotskiy@gmail.com>
2021-12-14 15:05:20 +0300

    upd

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "standard"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 13,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    "react/prop-types": "off",
-    indent: ["error", 4],
-    semi: ["error", "never"],
-    "space-before-function-paren": ["error", "never"],
-    quotes: ["error", "single", { allowTemplateLiterals: true }],
-  },
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: ["plugin:react/recommended", "standard"],
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 13,
+//     sourceType: "module",
+//   },
+//   plugins: ["react"],
+//   rules: {
+//     "react/prop-types": "off",
+//     indent: ["error", 4],
+//     semi: ["error", "never"],
+//     "space-before-function-paren": ["error", {anonymous:"always",named:"never"}],
+//     quotes: ["error", "single", { allowTemplateLiterals: true }],
+//   },
+// };
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,22 +1,49 @@
-import React, { useState } from 'react'
+import React, {useState, useEffect} from 'react'
 import api from '../api/index'
 import SearchStatus from './searchStatus'
 import User from './user'
 import Pagination from './pagination'
-import { paginate } from '../utils/paginate'
-
+import {paginate} from '../utils/paginate'
+import GroupList from './groupList'
+import PropTypes from 'prop-types'
+/* eslint-disable */
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll())
     const [currentPage, setCurrentPage] = useState(1)
-    const count = users.length
-    const pageSize = 4
+    const [professions, setProfessions] = useState()
+    const pageSize = 2
+    const [selectedProf, setSelectedProf] = useState()
 
-    const userCrop = paginate(users, currentPage, pageSize)
     const handleDelete = (userId) => {
         setUsers((prevState) => prevState.filter((user) => user._id !== userId))
     }
+
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex)
+    }
+    const filteredUsers = selectedProf
+        ? users.filter((user) => user.profession === selectedProf)
+        : users
+
+    const count = filteredUsers.length
+
+    const userCrop = paginate(filteredUsers, currentPage, pageSize)
+
+    if (userCrop.length === 0) {
+        handlePageChange(currentPage - 1)
+    }
+
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => {
+            setProfessions(data)
+        })
+    }, [])
+
+    const handleProfessionSelect = (item) => {
+        setSelectedProf(item)
+    }
+    const clearFilter = () => {
+        setSelectedProf()
     }
     if (count < 1) {
         return (
@@ -26,31 +53,55 @@ const Users = () => {
         )
     } else {
         return (
-            <>
-                <SearchStatus props={users.length} />
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">Имя</th>
-                            <th scope="col">Качества</th>
-                            <th scope="col">Профессия</th>
-                            <th scope="col">Встретился, раз</th>
-                            <th scope="col">Оценка</th>
-                            <th scope="col">Избранное</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <User users={userCrop} onDelete={handleDelete} />
-                    </tbody>
-                </table>
-                <Pagination
-                    itemsCount={count}
-                    pageSize={pageSize}
-                    currentPage={currentPage}
-                    onPageChange={handlePageChange}
-                />
-            </>
+            <div className="d-flex">
+                {professions && (
+                    <div className="d-flex flex-column flex-shrink-0 p-3">
+                        <GroupList
+                            selectedItem={selectedProf}
+                            items={professions}
+                            onItemSelect={handleProfessionSelect}
+                        />
+                        <button
+                            className="btn btn-secondary mt-2"
+                            onClick={clearFilter}
+                        >
+                            Очистить
+                        </button>
+                    </div>
+                )}
+                <div className="d-flex flex-column">
+                    <SearchStatus props={count}/>
+                    {count > 0 && (
+                        <table className="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">Имя</th>
+                                <th scope="col">Качества</th>
+                                <th scope="col">Профессия</th>
+                                <th scope="col">Встретился, раз</th>
+                                <th scope="col">Оценка</th>
+                                <th scope="col">Избранное</th>
+                                <th scope="col"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <User
+                                users={userCrop}
+                                onDelete={handleDelete}
+                            />
+                            </tbody>
+                        </table>
+                    )}
+                    <div className="d-flex justify-content-center">
+                        <Pagination
+                            itemsCount={count}
+                            pageSize={pageSize}
+                            currentPage={currentPage}
+                            onPageChange={handlePageChange}
+                        />
+                    </div>
+                </div>
+            </div>
         )
     }
 }
